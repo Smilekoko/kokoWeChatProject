91f074d036bf3b1d5a49ac43c249ea74a1889cf0
Smilekoko <qq179621228@gmail.com>
2023-05-19 14:36:14 +0800

    小程序页面生命周期
    onXxx(){} 系列回调方法说明

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,18 +1,23 @@
-// app.ts 为小程序逻辑
+// app.ts ：小程序逻辑
+// 每个小程序都需要在 app.js 中调用 App 方法注册小程序实例，绑定生命周期回调函数、错误监听和页面不存在监听函数等。
 App<IAppOption>({
-  globalData: {},
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    globalData: {},
+    onLaunch() {
+        // 展示本地存储能力
+        const logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        console.log(res.code)
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      },
-    })
-  },
+        // 登录
+        wx.login({
+            success: res => {
+                console.log(res.code)
+                // 发送 res.code 到后台换取 openId, sessionKey, unionId
+            },
+        })
+    },
+    //隐藏
+    onHide() {
+
+    },
 })
--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,49 +1,66 @@
-// index.ts
+// index.ts(TypeScript)：页面逻辑
 // 获取应用实例
 const app = getApp<IAppOption>()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs',
-    })
-  },
-  onLoad() {
-    // @ts-ignore
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile() {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    data: {
+        motto: 'Hello World',
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        canIUseGetUserProfile: false,
+        canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    },
+    // 事件处理函数
+    bindViewTap() {
+        wx.navigateTo({
+            url: '../logs/logs',
         })
-      }
-    })
-  },
-  getUserInfo(e: any) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+    },
+    // 页面创建时执行  
+    onLoad() {
+        // @ts-ignore
+        if (wx.getUserProfile) {
+            this.setData({
+                canIUseGetUserProfile: true
+            })
+        }
+    },
+    //页面首次渲染完毕时执行
+    onReady() { },
+    //页面从前台变为后台时执行
+    onHide() { },
+    //页面下拉刷新时执行 
+    onPullDownRefresh() { },
+    //页面触底时执行
+    onReachBottom() { },
+    //页面被用户分享时执行
+    onShareAppMessage() { },
+    //页面滚动时执行
+    onPageScroll() { },
+    //页面尺寸变化时执行
+    onResize() { },
+    getUserProfile() {
+        // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+        wx.getUserProfile({
+            desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+            success: (res) => {
+                console.log(res)
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            }
+        })
+    },
+    getUserInfo(e: any) {
+        // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+        console.log(e)
+        this.setData({
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
+        })
+    },
+    //页面消耗时执行
+    onUnload() { }
 })
